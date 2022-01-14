8d4d51370b0c445f1862f2de553924a1b956a1dd
koladev <onaelmangabo@gmail.com>
2022-01-14 16:39:21 +0100

    - feat: add user model auth settings

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
