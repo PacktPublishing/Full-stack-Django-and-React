ff9fca17bcc31ec3644d8e8e0df8192d93ac98e5
koladev <onaelmangabo@gmail.com>
2022-01-14 15:59:45 +0100

    - feat: register user apps into settings

--- core/user/apps.py
+++ core/user/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class UserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    name = 'core.user'
+    label = 'core_user'
