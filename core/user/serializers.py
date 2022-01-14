50d23ee71ed6edbeb0c1b3d3fd309040e4901cb9
koladev <onaelmangabo@gmail.com>
2022-01-15 00:53:59 +0100

    - feat: add user serializer

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -1,10 +1,15 @@
-from core.user.models import User
 from rest_framework import serializers
+
+from core.user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'is_active', 'created', 'updated']
-        read_only_field = ['is_active', 'created', 'updated']
+        fields = ['id', 'username', 'first_name', 'last_name', 'bio', 'avatar', 'email', 'is_active',
+                  'created', 'updated']
+        read_only_field = ['is_active']
