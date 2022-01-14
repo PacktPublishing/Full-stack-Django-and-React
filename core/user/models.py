1ba03efc492ab7bc7102ff8dd5801187635bc63a
koladev <onaelmangabo@gmail.com>
2022-01-15 00:50:18 +0100

    - feat: add bio and avatar field

--- core/user/models.py
+++ core/user/models.py
@@ -58,6 +58,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True)
+
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
 
