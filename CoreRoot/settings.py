d45ee8c5754e2b19ddb18aa6d21071a1f428ed6d
koladev <onaelmangabo@gmail.com>
2022-01-15 13:29:59 +0100

    - feat: registering DRF JWT app

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -130,3 +130,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core_user.User'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
