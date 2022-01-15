2a47f90347a76295162d0df1398bbe4b6222404e
koladev <onaelmangabo@gmail.com>
2022-01-15 13:56:23 +0100

    - feat: add registration viewset

--- core/auth/viewsets/register.py
+++ core/auth/viewsets/register.py
@@ -1,21 +1,18 @@
-# core/auth/viewsets
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ViewSet
 from rest_framework.permissions import AllowAny
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from core.auth.serializers import RegisterSerializer
 
-from optimus.authentication.serializers import RegisterSerializer
 
-
-class RegisterViewSet(ModelViewSet, TokenObtainPairView):
+class RegistrationViewSet(ViewSet):
     serializer_class = RegisterSerializer
     permission_classes = (AllowAny,)
     http_method_names = ['post']
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.serializer_class(data=request.data)
 
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
