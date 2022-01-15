4b43866be6624592c79e7e35856ed11aa83c2c1d
koladev <onaelmangabo@gmail.com>
2022-01-15 14:51:47 +0100

    - feat: add login endpoint

--- core/routers.py
+++ core/routers.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from core.user.viewsets import UserViewSet
-from core.auth.viewsets import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet, LoginViewSet
 
 router = routers.SimpleRouter()
 
@@ -9,6 +9,7 @@ router = routers.SimpleRouter()
 # ##################################################################### #
 
 router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+router.register(r'auth/login', LoginViewSet, basename='auth-login')
 
 
 # ##################################################################### #
