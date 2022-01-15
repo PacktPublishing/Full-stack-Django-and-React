6a3eaf72d2da064abc0928acf1e7f8bcbd8c57af
koladev <onaelmangabo@gmail.com>
2022-01-15 14:39:32 +0100

    - feat: add login feature

---
+++ core/auth/serializers/login.py
@@ -0,0 +1,24 @@
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+from django.contrib.auth.models import update_last_login
+
+from core.user.serializers import UserSerializer
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data['user'] = UserSerializer(self.user).data
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
+
+
