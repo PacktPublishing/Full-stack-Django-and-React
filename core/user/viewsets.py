88bec2c3abc64b9dcacccafe9d1bd8866d271012
koladev <onaelmangabo@gmail.com>
2022-01-15 01:08:35 +0100

    - fix: fix typo

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -13,7 +13,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.user.is_superuser:
             return User.objects.all()
-        return User.objects.exclude(user__is_superuser=True)
+        return User.objects.exclude(is_superuser=True)
 
     def get_object(self):
         obj = User.objects.get_object_by_public_id(self.kwargs['pk'])
