f4fe3e0576e6f9eebb0980dc074e0831aa895b66
koladev <onaelmangabo@gmail.com>
2022-02-02 17:32:37 +0100

    - feat: add post model

--- core/post/models.py
+++ core/post/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from core.abstract.models import AbstractModel, AbstractManager
+
+
+class PostManager(AbstractManager):
+    pass
+
+
+class Post(AbstractModel):
+    author = models.ForeignKey(to="core_user.User", on_delete=models.PROTECT)
+    body = models.TextField()
+    edited = models.BooleanField()
+
+    objects = PostManager()
+
+    def __str__(self):
+        return f"{self.author.name}"
+
+    class Meta:
+        db_table = "'core.post'"
