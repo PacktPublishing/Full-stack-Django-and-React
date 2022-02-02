73632876ab0ec42eb36a4332d71dbba83d0aef7c
koladev <onaelmangabo@gmail.com>
2022-02-02 21:17:42 +0100

    - feat: add update and delete methods

--- core/post/serializers.py
+++ core/post/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from core.abstract.serializers import AbstractSerializer
 from core.post.models import Post
@@ -7,6 +8,20 @@ from core.user.models import User
 
 class PostSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
+
+    def validate_author(self, value):
+        if self.context["request"].user != value:
+            raise ValidationError("You can't create a post for another user.")
+        return value
+
+    def update(self, instance, validated_data):
+        if not instance.edited:
+            validated_data['edited'] = True
+
+        instance = super().update(instance, validated_data)
+
+        return instance
+
 
     class Meta:
         model = Post
--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -1,3 +1,6 @@
+from rest_framework.response import Response
+from rest_framework import status
+
 from core.abstract.viewsets import AbstractViewSet
 from core.post.models import Post
 from core.post.serializers import PostSerializer
@@ -5,7 +8,7 @@ from core.auth.permissions import UserPermission
 
 
 class PostViewSet(AbstractViewSet):
-    http_method_names = ('post', 'get')
+    http_method_names = ('post', 'get', 'put', 'delete')
     permission_classes = (UserPermission,)
     serializer_class = PostSerializer
 
@@ -18,3 +21,11 @@ class PostViewSet(AbstractViewSet):
         self.check_object_permissions(self.request, obj)
 
         return obj
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
