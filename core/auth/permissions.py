82631a249d5528bd835412ae751736337b891704
koladev <onaelmangabo@gmail.com>
2022-03-02 04:47:16 +0100

    - fix: fix typo

--- core/auth/permissions.py
+++ core/auth/permissions.py
@@ -6,7 +6,13 @@ class UserPermission(BasePermission):
         if request.user.is_anonymous:
             return request.method in SAFE_METHODS
 
-        if view.basename in ["post", "post-comment"]:
+        if view.basename in ["post"]:
+            return bool(request.user and request.user.is_authenticated)
+
+        if view.basename in ["post-comment"]:
+            if request.method in ['DELETE']:
+                return bool(request.user.is_superuser or request.user in [obj.author, obj.post.author])
+
             return bool(request.user and request.user.is_authenticated)
 
         return False
