7febba182ad588975d89f6f6fd877d08e3ed5cc9
koladev <onaelmangabo@gmail.com>
2022-03-13 22:40:35 +0100

    - feat: add fixtures

--- core/post/tests.py
+++ core/post/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from core.fixtures.user import user
+from core.post.models import Post
+
+@pytest.mark.django_db
+def test_create_post(user):
+    post = Post.objects.create(author=user, body="Test Post Body")
+    assert post.body == "Test Post Body"
