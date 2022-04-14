c1317114ed795e9fa9af3a603f8afd76a6d88bc7
koladev <onaelmangabo@gmail.com>
2022-04-14 22:25:06 +0100

    🚧 add bootstrap config

--- social-media-react/src/index.js
+++ social-media-react/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
