83033c36092ac40d130cd5dcb99b4230801515c7
koladev <onaelmangabo@gmail.com>
2022-04-14 22:28:01 +0100

    🛂 add react router

--- social-media-react/src/index.js
+++ social-media-react/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistor } from './store';
@@ -17,13 +17,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
