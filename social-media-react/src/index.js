d3d99f4e3472fece5552fb5829d3104b794e07a5
koladev <onaelmangabo@gmail.com>
2022-04-27 14:17:18 +0100

    ✨ add routing

--- social-media-react/src/index.js
+++ social-media-react/src/index.js
@@ -1,26 +1,16 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistor } from './store';
-import store from './store';
+import "./index.css";
+import App from "./App";
 
 // Creating the root application component
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
