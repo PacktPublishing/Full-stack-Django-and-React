6774cbaa6d426eea8885e2234ddfdf3ea72ce1b0
koladev <onaelmangabo@gmail.com>
2022-06-15 14:23:14 +0100

    ✨ add user actions hook

---
+++ social-media-react/src/hooks/user.actions.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export function useUserActions() {
+  const navigate = useNavigate();
+  const baseURL = "http://localhost:8000/api";
+
+  return {
+    login,
+    register,
+    logout,
+    getUser,
+    getAccessToken,
+    getRefreshToken,
+  };
+
+  // Login the user
+  function login(data) {
+    return axios.post(`${baseURL}/auth/login/`, data).then((res) => {
+      // Registering the account and tokens in the store
+      localStorage.setItem(
+        "auth",
+        JSON.stringify({
+          access: res.data.access,
+          refresh: res.data.refresh,
+          user: res.data.user,
+        })
+      );
+      navigate("/");
+    });
+  }
+
+  // Register the user
+  function register(data) {
+    return axios.post(`${baseURL}/auth/register/`, data).then((res) => {
+      // Registering the account and tokens in the store
+      localStorage.setItem(
+        "auth",
+        JSON.stringify({
+          access: res.data.access,
+          refresh: res.data.refresh,
+          user: res.data.user,
+        })
+      );
+      navigate("/");
+    });
+  }
+
+  // Logout the user
+  function logout() {
+    localStorage.removeItem("auth");
+    navigate("/login");
+  }
+
+  // Get the user
+  function getUser() {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    return auth.user;
+  }
+
+  // Get the access token
+  function getAccessToken() {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    return auth.access;
+  }
+
+  // Get the refresh token
+  function getRefreshToken() {
+    const auth = JSON.parse(localStorage.getItem("auth"));
+    return auth.refresh;
+  }
+}
