4cf5d354619c7a74363255b6b2096ef0d703415d
koladev <onaelmangabo@gmail.com>
2022-07-07 00:24:25 +0100

    ✨ add logout feature

--- social-media-react/src/components/Navbar.jsx
+++ social-media-react/src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import { randomAvatar } from "../utils";
 
 import { Navbar, Container, Image, NavDropdown, Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function Navigationbar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("auth");
+    navigate("/login/");
+  };
+
   return (
     <Navbar bg="primary" variant="dark">
       <Container>
@@ -23,7 +30,7 @@ function Navigationbar() {
               }
             >
               <NavDropdown.Item href="#">Profile</NavDropdown.Item>
-              <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>Logout</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
