0d003e30c11996c77e9eb6a7db575b24437b6798
koladev <onaelmangabo@gmail.com>
2022-07-21 14:11:47 +0100

    :sparkles: add like fields to comment serializer

--- core/comment/serializers.py
+++ core/comment/serializers.py
@@ -11,6 +11,20 @@ from core.post.models import Post
 class CommentSerializer(AbstractSerializer):
     author = serializers.SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
     post = serializers.SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
+    liked = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
+
+    def get_liked(self, instance):
+
+        request = self.context.get('request', None)
+
+        if request is None or request.user.is_anonymous:
+            return False
+
+        return request.user.has_liked_comment(instance)
+
+    def get_likes_count(self, instance):
+        return instance.commented_by.count()
 
     def validate_author(self, value):
         if self.context["request"].user != value:
