dc50f8b25f5f91f5bfaf72fb5ca142853ab7733f
koladev <onaelmangabo@gmail.com>
2022-07-24 21:57:30 +0100

    ✨ fetch data from author

--- social-media-react/src/pages/Profiile.jsx
+++ social-media-react/src/pages/Profiile.jsx
@@ -1,32 +1,35 @@
 import React from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import Layout from "../components/Layout";
 import ProfileDetails from "../components/profile/ProfileDetails";
 import useSWR from "swr";
-import {fetcher} from "../helpers/axios";
-import {Post} from "../components/posts";
-import {Row} from "react-bootstrap";
-
+import { fetcher } from "../helpers/axios";
+import { Post } from "../components/posts";
+import { Row, Col } from "react-bootstrap";
 
 function Profile() {
-    const {profileId} = useParams();
+  const { profileId } = useParams();
 
-    const posts = useSWR("/post/", fetcher, {
-        refreshInterval: 20000,
-    });
+  const posts = useSWR(`/post/?author=${profileId}`, fetcher, {
+    refreshInterval: 20000
+  });
 
-    return (
-        <Layout hasNavigationBack>
-            <ProfileDetails/>
-            <div>
-                <Row className="my-4">
-                    {posts.data?.results.map((post, index) => (
-                        <Post key={index} post={post} refresh={posts.mutate}/>
-                    ))}
-                </Row>
-            </div>
-        </Layout>
-    )
+  return (
+    <Layout hasNavigationBack>
+      <Row className="justify-content-evenly">
+        <Col sm={9}>
+          <ProfileDetails />
+          <div>
+            <Row className="my-4">
+              {posts.data?.results.map((post, index) => (
+                <Post key={index} post={post} refresh={posts.mutate} />
+              ))}
+            </Row>
+          </div>
+        </Col>
+      </Row>
+    </Layout>
+  );
 }
 
 export default Profile;
