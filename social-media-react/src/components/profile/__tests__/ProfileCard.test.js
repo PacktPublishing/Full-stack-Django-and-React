43143112c8cfd7c13b1153b04b3706f552e2df29
koladev <onaelmangabo@gmail.com>
2022-08-21 00:37:38 +0100

    :recycle: move snapshot test on ProfileCard

--- social-media-react/src/components/profile/__tests__/ProfileCard.test.js
+++ social-media-react/src/components/profile/__tests__/ProfileCard.test.js
@@ -1,8 +1,21 @@
 import { render, screen, fireEvent } from "../../../helpers/test-utils";
-import userFixtures from "../../../helpers/fixtures/user";
+import TestRenderer from "react-test-renderer";
 import ProfileCard from "../ProfileCard";
+import { BrowserRouter } from "react-router-dom";
 
-const userData = userFixtures();
+const userData = {
+  id: "0590cd67-eacd-4299-8413-605bd547ea17",
+  first_name: "Mossie",
+  last_name: "Murphy",
+  name: "Mossie Murphy",
+  post_count: 3,
+  email: "Mossie@yopmail.com",
+  bio: "Omnis necessitatibus facere vel in est provident sunt tempora earum accusantium debitis vel est architecto minima quis sint et asperiores.",
+  username: "MossieMurphy",
+  avatar: null,
+  created: "2022-08-19T17:31:03.310Z",
+  updated: "2022-08-20T07:38:47.631Z",
+};
 
 test("Render ProfileCard component", () => {
   render(<ProfileCard user={userData} />);
@@ -12,4 +25,13 @@ test("Render ProfileCard component", () => {
 
   const nameElement = screen.getByText(userData.name);
   expect(nameElement).toBeInTheDocument();
+});
+
+test("Profile Card snapshot", () => {
+  const profileCardDomTree = TestRenderer.create(
+    <BrowserRouter>
+      <ProfileCard user={userData} />
+    </BrowserRouter>
+  ).toJSON();
+  expect(profileCardDomTree).toMatchSnapshot();
 });
