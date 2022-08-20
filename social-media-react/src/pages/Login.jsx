8d4cb0dc570569e149f6ed20026fc921bc185910
koladev <onaelmangabo@gmail.com>
2022-08-20 14:20:59 +0100

    :recycle: rename forms folder to authentication

--- social-media-react/src/pages/Login.jsx
+++ social-media-react/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LoginForm from "../components/forms/LoginForm";
+import LoginForm from "../components/authentication/LoginForm";
 
 function Login() {
   return (
