543747dc04b4198e55639f34e3702715637b6598
koladev <onaelmangabo@gmail.com>
2022-11-03 22:29:27 +0100

    ✨ add webpack

---
+++ social-media-react/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: ["", ".js", ".jsx"],
+  },
+};
