d717975a8613743e3efb01e4b45c3b78188ca58b
koladev <onaelmangabo@gmail.com>
2022-11-03 02:13:16 +0100

    :bug: fix bug

--- core/comment/viewsets.py
+++ core/comment/viewsets.py
@@ -1,4 +1,5 @@
 from django.http.response import Http404
+from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
