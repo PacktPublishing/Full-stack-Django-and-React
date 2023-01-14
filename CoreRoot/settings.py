f7d3c83c3bbd0460c10a0e930473a7800a326d15
Mangabo  Kolawole <32314456+koladev32@users.noreply.github.com>
2023-01-14 21:20:48 +0100

    Update settings.py

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -166,7 +166,7 @@ DEFAULT_AVATAR_URL = "https://avatars.dicebear.com/api/identicon/.svg"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
